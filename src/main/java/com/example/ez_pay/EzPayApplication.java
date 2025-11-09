aa99ef7821f965fbc003131c99aad5f0d58c5efd
MontillaTomas <montilla.tom1@gmail.com>
2025-11-09 20:41:54 -0300

    First commit

---
+++ src/main/java/com/example/ez_pay/EzPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ez_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzPayApplication.class, args);
+	}
+
+}
